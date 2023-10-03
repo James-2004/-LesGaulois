89fbf6f03d78d4e9c31fa392f15dc41685ec2d1f
James Saba <sbj4618a@ens.fsi>
2023-10-03 09:46:02 +0200

    TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,15 +3,30 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	public int nbVillageois=0;
+	public int nbVillageoisMaximum;
+	Gaulois[] villageois = new Gaulois[nbVillageoisMaximum];
 	
 	public Village(String nom) {
 		this.nom = nom;
+		this.nbVillageois = nbVillageois;
 	}
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
 	public String getNom() {
 		return nom;
+	}
+	public void ajouterHabitant(Gaulois unGaulois) {
+		villageois[nbVillageois] = unGaulois;
+		nbVillageois+=1;
+	}
+	public Gaulois trouverHabitant(int nbRefVillageois) {
+		return villageois[nbRefVillageois];
+	}
+	public static void main(String[] args) {
+		Village VillagedesIrréductibles = new Village("VillagedesIrréductibles",30);
+		
 	}
 }
 
